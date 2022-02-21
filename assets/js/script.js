b208c53a8496e65d375d6b48d73f1e30fd7129ed
AntunesJohann <johann.antunes@gmail.com>
2022-02-21 03:21:23 -0300

    Aprimorada consistência de código.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,8 @@ theme_toggle.addEventListener('change', () => {
 	if (theme_toggle.checked) {
 		rootElement.style.setProperty('--fontColor', '#e2e2e2');
 		rootElement.style.setProperty('--bgColor', '#202020');
-	} else {
+	}
+	else {
 		rootElement.style.setProperty('--fontColor', '#202020');
 		rootElement.style.setProperty('--bgColor', '#e2e2e2');
 	}
@@ -37,10 +38,8 @@ function HidePage() {
 
 
 setTimeout(() => {
-	if (firstPage.style.display = 'grid') {
-		firstPage.style.display = 'none';
-		secondPage.style.display = 'grid';
-	};
+	firstPage.style.display = 'none';
+	secondPage.style.display = 'grid';
 }, 12500)
 
 // document.addEventListener('keydown', function (Key) {
